51f1e0de10a0f82a3380a3b5a58c0f253748fc83
Pratik Murali <pratikm@netapp.com>
2015-05-04 02:06:08 -0700

    Modify signature to include chunks.

--- src/main/java/com/pratik/objectstore/service/ObjectStoreController.java
+++ src/main/java/com/pratik/objectstore/service/ObjectStoreController.java
@@ -1,40 +1,43 @@
 package com.pratik.objectstore.service;
 
-import java.io.File;
-
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pratik.objectstore.api.Upload;
+import com.pratik.objectstore.model.UploadRequest;
+import com.pratik.objectstore.model.UploadResponse;
+import com.pratik.objectstore.model.Version;
 
 @RestController
 @EnableAutoConfiguration
-@RequestMapping ("/largeobject")
 public class ObjectStoreController {
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	Upload uploadService;
 	
+	//Spring boot automatically uses Jackson to marshal out JSON instances.
 	@RequestMapping(value = "/version", method = RequestMethod.GET)
-	public String getVersion() {
+	public Version getVersion() {
 
-		return "1.0";
+		return new Version("1.0");
 
 	}
 	
-	@RequestMapping(value = "/user", method = RequestMethod.POST)
-	public void putObject(String mimeType, String absoluteFilePath) {
-		
-		uploadService.uploadLargeObject(mimeType, absoluteFilePath);
-		
-	}
-	
-	File getObject(String key) {
-		
-		return null;
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	public UploadResponse putObject(@RequestBody UploadRequest request) {
+		logger.debug("User sent Mime Type = "+request.getMimeType());
+		logger.debug("User sent Absolute File Path = "+StringEscapeUtils.escapeJson(request.getAbsFilePath()));
+		logger.debug("User sent number of chunks = "+request.getNoOfChunks());
+		return uploadService.uploadLargeObject(request.getMimeType(), request.getAbsFilePath(), request.getNoOfChunks());
 		
 	}
 
